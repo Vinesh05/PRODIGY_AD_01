3c8fe5f7fdb8a7d02072b0a73293d0967114cd96
Vinesh05 <vineshchtr@gmail.com>
2023-07-05 19:10:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simple_calculator/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.simple_calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simple_calculator.ui.theme.Simple_CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Simple_CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Calculator App")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Simple_CalculatorTheme {
+        Greeting("Calculator App")
+    }
+}
